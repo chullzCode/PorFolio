4d5fd7f768bb422be4b8a66e7606337bfc612e87
chullzCode <sohailzahid697@gmail.com>
2024-02-05 18:14:10 +0100

    I styled Nav section

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
-let pic = document.getElementById("profile-pic")
-function img(){
-    // pic.src = "images/profolio.png","images/icon.png"
-    if (pic.src.match("images/icon.png")){
-        pic.src = "images/profolio.png"
-    } else {
-        pic.src = "images/icon.png"
-    }
-}
+// let pic = document.getElementById("profile-pic")
+// function img(){
+//     // pic.src = "images/profolio.png","images/icon.png"
+//     if (pic.src.match("images/icon.png")){
+//         pic.src = "images/profolio.png"
+//     } else {
+//         pic.src = "images/icon.png"
+//     }
+// }
