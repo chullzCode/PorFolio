2bb0477dedae99329f022309126665f83103c21a
chullzCode <sohailzahid697@gmail.com>
2024-02-10 19:34:30 +0100

    added some info in about me section

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
+
+let tabLinks = document.getElementsByClassName("tab-links")
+let tabContents = document.getElementsByClassName("tab-contents")
+
+
+function openTab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link")
+    }
+    for(tabContent of tabContents){
+        tabContents.classList.remove("active-Tab")
+    }
+    event.currentTarget.classList.add("active-link")
+}
